75fc130a8eb5f0c5417db096506235f1b084b5e1
rosstafarian <rosstafarian1@gmail.com>
2014-04-30 23:28:13 -0500

    Contact Manager application basics added

---
+++ app/controllers/Contacts.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.mvc.Controller
+
+object Contacts extends Controller {
+
+  def index = TODO
+
+  def create = TODO
+
+  def edit(id: Long) = TODO
+
+  def update(id: Long) = TODO
+
+  def delete(id: Long) = TODO
+
+}
