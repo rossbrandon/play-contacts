6fafba0fa1e037da1f40a763bbe861831e0c6afc
rosstafarian <rosstafarian1@gmail.com>
2014-04-30 23:13:35 -0500

    Test routes added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,22 @@
 package controllers
 
 import play.api.mvc._
-import play.api.Play
 
 object Application extends Controller {
 
   def index = Action {
-    val setting = Play.current.configuration.getString("app.setting")
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def token(id: Long) = Action {
+    Ok(s"This is a token based route: $id")
+  }
+
+  def regex(id: Long) = Action {
+    Ok(s"This is a regex based route: $id")
+  }
+
+  def clob(path: String) = Action {
+    Ok(s"This is a clobbed route: $path")
+  }
 }
